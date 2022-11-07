02d08e2536c88261534ed4ade8397f1f2b84130f
Matthew Cochran <matthew@cochranweb.com>
2022-11-07 16:11:52 -0500

    service proxy wip

---
+++ src/Tests/ServiceProxyTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) TruthShield, LLC. All rights reserved.
+namespace Applinate.Foundation.Test
+{
+    using Applinate.Test;
+
+    public sealed class ServiceProxyTest: TestBase
+    {
+        [Fact]
+        public async Task DispatchRequestFromInterface()
+        {
+            // TODO: create all service proxies ahead of time.
+
+            // TOOD: add implementation option for interfaces.
+
+            var proxy = ServiceProvider.Locate<IMy_0010_Service>();// ServiceProxyFactory.Build<IMy_0010_Service>();
+            var response = proxy.HandleRequestAsync(new());
+
+            _ = response;
+        }
+
+        [Fact]
+        public async Task HandleRequestFromInterfaceImplementation()
+        {
+            var proxy = ServiceProvider.Locate<IMy_011_Service>();// ServiceProxyFactory.Build<IMy_0010_Service>();
+            var response = await proxy.HandleRequestAsync(new()
+            {
+                Value = 123
+            });
+
+            response.Value.Should().Be(123);
+        }
+
+    }
+}
